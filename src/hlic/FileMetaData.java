4e098e4a9112687bcb778fe3f92eaca06fa1ec9b
MohamedMosAmin <mohamed.m.amin@hotmail.com>
2012-12-07 21:45:43 +0200

    Amin: first report done

--- src/hlic/FileMetaData.java
+++ src/hlic/FileMetaData.java
@@ -7,10 +7,27 @@ public class FileMetaData {
 	private int fileSize;
 	
 	
-	public FileMetaData(boolean v){
+	public FileMetaData(boolean v, int fSize){
 		this.valid = v;
 		this.linksVaild = true;
 		this.numberOfOcc = 1;
+		this.fileSize = fSize;
+	}
+	
+	public boolean getValid(){
+		return this.valid;
+	}
+	
+	public boolean getLinksValid(){
+		return this.linksVaild;
+	}
+	
+	public int getNumOfOcc(){
+		return this.numberOfOcc;
+	}
+	
+	public int getFileSize(){
+		return this.fileSize;
 	}
 	
 	public void increaseOcc(){
